c726d1f92688af2d914fb041753064460512dd5d
nurselisar <nurselisarr@gmail.com>
2021-03-28 00:14:13 +0300

    Authentication, authorization are added. jwt token generation is added for user model.

--- src/main/java/com/aidassist/bl/UserService.java
+++ src/main/java/com/aidassist/bl/UserService.java
@@ -4,12 +4,17 @@ import com.aidassist.model.User;
 import com.aidassist.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -30,5 +35,14 @@ public class UserService {
         User createdUser = userRepository.save(user);
         logger.info("User saved {}", createdUser);
         return createdUser;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String nationalIdentityNo) throws UsernameNotFoundException {
+        return userRepository.findById(nationalIdentityNo).get();
+    }
+
+    public Optional<User> findUserById(String nationalIdentityNo) {
+        return userRepository.findById(nationalIdentityNo);
     }
 }
--- src/main/java/com/aidassist/controller/UserController.java
+++ src/main/java/com/aidassist/controller/UserController.java
@@ -1,12 +1,24 @@
 package com.aidassist.controller;
 
+import com.aidassist.bl.UserService;
+import com.aidassist.jwt.JwtUtil;
 import com.aidassist.model.User;
+import com.aidassist.model.UserResponse;
+import com.aidassist.model.UserResponseError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("user")
@@ -15,26 +27,56 @@ public class UserController {
     // TODO this will be deleted after db configurations
     Map<String, User> userMap = new HashMap<String, User>();
 
-    @CrossOrigin
-    @PostMapping(value = "/signUp", produces = "application/json")
-    public ResponseEntity<String> register(@RequestBody User user) {
+    @Autowired
+    private JwtUtil jwtUtil;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @CrossOrigin
+    @PostMapping(value = "/signUp", produces = "application/json", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserResponse> register(@RequestBody User user) {
+        UserResponse userResponse = new UserResponse();
         // Check user exists
-        if (userMap.containsKey(user.getNationalIdentityNo())) {
-            return new ResponseEntity<String>("User already registered!",HttpStatus.CONFLICT);
+        Optional<User> userOptional = userService.findUserById(user.getNationalIdentityNo());
+        if (userOptional.isPresent()) {
+            userResponse.setError(new UserResponseError("ID_EXISTS"));
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
         }
-        userMap.put(user.getNationalIdentityNo(), user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        try {
+            userService.createUser(user);
+        } catch (Exception e) {
+            userResponse.setError(new UserResponseError(""));
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        }
+        final UserDetails userDetails = userService.loadUserByUsername(user.getNationalIdentityNo());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        userResponse.setIdToken(jwt);
+
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @CrossOrigin
     @PostMapping(value = "/signIn", produces = "application/json")
-    public ResponseEntity<String> signIn(@RequestParam("id") String id, @RequestParam("password") String password) {
-        if (userMap.containsKey(id) && userMap.get(id).getPassword().equals(password)) {
-            return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<UserResponse> signIn(@RequestBody User user) throws Exception {
+        UserResponse userResponse = new UserResponse();
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getNationalIdentityNo(), user.getPassword()));
+        } catch (InternalAuthenticationServiceException e) {
+            userResponse.setError(new UserResponseError("ID_NOT_FOUND"));
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
+        } catch (BadCredentialsException ex) {
+            userResponse.setError(new UserResponseError("INVALID_PASSWORD"));
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
         }
+        final UserDetails userDetails = userService.loadUserByUsername(user.getNationalIdentityNo());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        userResponse.setIdToken(jwt);
 
-        return  new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
 
